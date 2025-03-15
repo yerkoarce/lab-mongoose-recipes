a6c6a3a7de06640d8b5bb735f8b356e75f3ed6c8
Yerko Arce <yerko.arce.galaz@gmail.com>
2025-03-15 09:53:36 -0300

    Iteration 4 completed, update recipe

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { ReturnDocument } = require('mongodb');
 
 const MONGODB_URI = 'mongodb+srv://yerkoarcegalaz:Brd7tkNL2GATAMxX@openbootcamp.8xxrw.mongodb.net/?retryWrites=true&w=majority&appName=openbootcamp'
 
 
+async function manageRecipes() {
+  try {
+    // Connection to the database "recipe-app"
+    await mongoose.connect(MONGODB_URI);
+    console.log(`Connected to the database: "${mongoose.connection.name}"`);
 
+    // Before adding any recipes to the database, let's remove all existing ones
+    await Recipe.deleteMany();
+
+    // Insert new recipes
+    const recipes = await Recipe.insertMany(data)
+    console.log('Recipes created!!')
+    recipes.forEach(recipe => {
+      console.log(recipe.title)
+    })
+
+    // Update recipe duration
+    await Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, {new: true})
+      .then(updateRecipe => {
+        if (updateRecipe) {
+          console.log('Recipe update: ', updateRecipe)
+        } else {
+          console.log("Don't find recipe")
+        }
+      })
+      .catch(error => {
+        console.error('Error to update recipe: ', error)
+      })
+
+    // Disconnect from the database
+    await mongoose.disconnect();
+
+  } catch (error) {
+    console.error('Error: ', error);
+  }
+}
+
+manageRecipes()
+
+/*
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
@@ -19,21 +59,17 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-    Recipe.create({
-      title: 'Primera receta',
-      level: 'Amateur Chef',
-      ingredients: ['algo', 'algo mas', 'Otra cosa mas'],
-      cuisine: 'Cocina',
-      dishType: 'breakfast',
-      duration: 5,
-      creator: 'Yerko Arce'
-      })
-      .then(recipe => {
-        console.log('Recipe created', recipe.title)
+    Recipe.insertMany(data)
+      .then(recipes => {
+        console.log('Recipes created')
+        recipes.forEach(recipe => {
+          console.log(recipe.title)
+        })
         mongoose.connection.close()
       })
-      .catch(error => console.log(`Error creating recipe: ${error}`));
+      .catch(error => console.log(`Error adding recipe: ${error}`));
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+*/
