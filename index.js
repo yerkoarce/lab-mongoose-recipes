c45112d11c75d67d38027cdc28d509adb69e6ca1
Yerko Arce <yerko.arce.galaz@gmail.com>
2025-03-14 23:38:07 -0300

    Iteration 2 completed

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb+srv://yerkoarcegalaz:Brd7tkNL2GATAMxX@openbootcamp.8xxrw.mongodb.net/?retryWrites=true&w=majority&appName=openbootcamp'
+
+
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,6 +19,20 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: 'Primera receta',
+      level: 'Amateur Chef',
+      ingredients: ['algo', 'algo mas', 'Otra cosa mas'],
+      cuisine: 'Cocina',
+      dishType: 'breakfast',
+      duration: 5,
+      creator: 'Yerko Arce'
+      })
+      .then(recipe => {
+        console.log('Recipe created', recipe.title)
+        mongoose.connection.close()
+      })
+      .catch(error => console.log(`Error creating recipe: ${error}`));
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
